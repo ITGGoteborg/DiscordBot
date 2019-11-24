eb2e64917395ff7ac520cf156a292ec0b2ee2760
Vifie180 <vidar.p@hotmail.se>
2019-11-24 19:41:53 +0100

    Updated jokes

--- bot.py
+++ bot.py
@@ -36,11 +36,11 @@ async def on_message(message):
     programming_jokes = [
         "How many programmers does it take to screw in a light bulb? \n\nNone. It's a hardware problem.",
         "A programmer puts two glasses on his bedside table before going to sleep. A full one, in case he gets thirsty, and an empty one, in case he doesn’t.",
-        "Java and C were telling jokes. It was C's turn, so he writes something on the wall, points to it and says \"Do you get the reference?\" But Java didn't.",
+        "Java and C were telling jokes. It was C's turn, so he writes something on the wall, points to it and says \"Do you get the reference?\" \nBut Java didn't.",
         "There are 10 kinds of people in this world: Those who understand binary, those who don't, and those who weren't expecting a base 3 joke.",
         "A programmer is heading out to the grocery store, so his wife tells him \"get a gallon of milk, and if they have eggs, get a dozen.\" He returns with 13 gallons of milk.",
-        "What's the best thing thing about UDP jokes? \nI don't care if you get them",
-        "What's the best part about TCP jokes? \nI get to keep telling them until you get them.",
+        "What's the best thing thing about UDP jokes? \n\nI don't care if you get them",
+        "What's the best part about TCP jokes? \n\nI get to keep telling them until you get them.",
         "Your mama's so FAT she can't save files bigger than 4GB.",
         "In order to understand recursion you must first understand recursion."
     ]
