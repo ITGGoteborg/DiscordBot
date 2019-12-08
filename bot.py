b74fa1bb29544ac7a9098feb424e72c3c3d79552
Vifie180 <vidar.p@hotmail.se>
2019-12-08 21:06:12 +0100

    Added 8ball and help messages

--- bot.py
+++ bot.py
@@ -55,9 +55,34 @@ def plus(content):
         return
 
 #Commands and responses for client
-@client.command(name='ping')
+@client.command(name='ping', help="Returns latency of Bot.")
 async def ping(ctx):
     await ctx.send(f"Pong! {round(client.latency * 1000)}ms")
+
+@client.command(aliases = ["8ball"], help="The Magic 8 Ball has all the answers to life's questions.")
+async def _8ball(ctx, *, question):
+    responses = [
+        "It is certain.",
+        "It is decidedly so.",
+        "Without a doubt.",
+        "Yes - definitely.",
+        "You may rely on it.",
+        "As I see it, yes.",
+        "Most likely.",
+        "Outlook good.",
+        "Yes.",
+        "Signs point to yes.",
+        "Reply hazy, try again.",
+        "Ask again later.",
+        "Better not tell you now.",
+        "Cannot predict now.",
+        "Concentrate and ask again.",
+        "Don't count on it.",
+        "My reply is no.",
+        "My sources say no.",
+        "Outlook not so good.",
+        "Very doubtful."]
+    await ctx.send(f"Question: {question}\nAnswer: {random.choice(responses)}")
 
 """ @client.event
 async def on_message(message):
